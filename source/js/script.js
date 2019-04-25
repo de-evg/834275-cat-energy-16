4e1502203de345ee072febde8c41b80f779de81c
de-evg <d.e.min@yandex.ru>
2019-04-25 14:50:52 +0400

    добавил input состояние ошибки при незаполненной форме

--- source/js/script.js
+++ source/js/script.js
@@ -1,57 +1,72 @@
 // Переключение главного меню
-var navMain = document.querySelector(".main-menu");
-var navToggle = document.querySelector(".burger-menu");
+if (document.querySelector(".main-menu") !== null) {
+  var navMain = document.querySelector(".main-menu");
+  var navToggle = document.querySelector(".burger-menu");
 
-navMain.classList.remove("main-menu--no-js");
-navToggle.classList.remove("burger-menu--no-js");
+  navMain.classList.remove("main-menu--no-js");
+  navToggle.classList.remove("burger-menu--no-js");
 
-navToggle.addEventListener("click", function() {
-  if (navToggle.classList.contains("burger-menu--closed")) {
-    navToggle.classList.remove("burger-menu--closed");
-    navToggle.classList.add("burger-menu--opened");
-  } else {
-    navToggle.classList.add("burger-menu--closed");
-    navToggle.classList.remove("burger-menu--opened");
-  }
+  navToggle.addEventListener("click", function() {
+    if (navToggle.classList.contains("burger-menu--closed")) {
+      navToggle.classList.remove("burger-menu--closed");
+      navToggle.classList.add("burger-menu--opened");
+    } else {
+      navToggle.classList.add("burger-menu--closed");
+      navToggle.classList.remove("burger-menu--opened");
+    }
 
-  if (navMain.classList.contains("main-menu--closed")) {
-    navMain.classList.remove("main-menu--closed");
-    navMain.classList.add("main-menu--opened");
-  } else {
-    navMain.classList.add("main-menu--closed");
-    navMain.classList.remove("main-menu--opened");
-  }
-});
+    if (navMain.classList.contains("main-menu--closed")) {
+      navMain.classList.remove("main-menu--closed");
+      navMain.classList.add("main-menu--opened");
+    } else {
+      navMain.classList.add("main-menu--closed");
+      navMain.classList.remove("main-menu--opened");
+    }
+  });
+}
 
 // Переключение слайдера в мобильной версии
-var sliderButton = document.querySelectorAll(".slider__button");
-var sliderToggle = document.querySelector(".slider__toggle-item")
-var sliderImage = document.querySelectorAll(".slider__image");
+if (document.querySelector(".slider") !== null) {
+  var sliderButton = document.querySelectorAll(".slider__button");
+  var sliderToggle = document.querySelector(".slider__toggle-item")
+  var sliderImage = document.querySelectorAll(".slider__image");
 
-sliderButton[1].addEventListener("click", function() {
-  if (!sliderButton[1].classList.contains("slider__button--current")) {
-    sliderButton[0].classList.remove("slider__button--current");
-    sliderButton[1].classList.add("slider__button--current");
-    sliderToggle.classList.toggle("slider__toggle-item--right");
-    if (!sliderImage[1].classList.contains("slider__image--current")) {
-      sliderImage[0].classList.remove("slider__image--current");
-      sliderImage[1].classList.add("slider__image--current");
-    };
+  sliderButton[1].addEventListener("click", function() {
+    if (!sliderButton[1].classList.contains("slider__button--current")) {
+      sliderButton[0].classList.remove("slider__button--current");
+      sliderButton[1].classList.add("slider__button--current");
+      sliderToggle.classList.toggle("slider__toggle-item--right");
+      if (!sliderImage[1].classList.contains("slider__image--current")) {
+        sliderImage[0].classList.remove("slider__image--current");
+        sliderImage[1].classList.add("slider__image--current");
+      };
+    }
+  })
+
+  sliderButton[0].addEventListener("click", function() {
+    if (!sliderButton[0].classList.contains("slider__button--current")) {
+      sliderButton[1].classList.remove("slider__button--current");
+      sliderButton[0].classList.add("slider__button--current");
+      sliderToggle.classList.toggle("slider__toggle-item--right");
+      if (!sliderImage[0].classList.contains("slider__image--current")) {
+        sliderImage[1].classList.remove("slider__image--current");
+        sliderImage[0].classList.add("slider__image--current");
+      };
+    }
+  })
+}
+
+// Ошибка при незаполненной форме заполнении формы
+var formSubmit = document.querySelector(".program-form__button-submit");
+var requiredInput = document.querySelectorAll("[required]");
+
+formSubmit.addEventListener("click", function() {
+  for (var i = 0; i < requiredInput.length; i++) {
+    if (requiredInput[i].value == "") {
+      requiredInput[i].classList.add("program-form__input--error");
+      requiredInput[i].focus();
+    } else {
+      requiredInput[i].classList.remove("program-form__input--error");
+    }
   }
 })
-
-sliderButton[0].addEventListener("click", function() {
-  if (!sliderButton[0].classList.contains("slider__button--current")) {
-    sliderButton[1].classList.remove("slider__button--current");
-    sliderButton[0].classList.add("slider__button--current");
-    sliderToggle.classList.toggle("slider__toggle-item--right");
-    if (!sliderImage[0].classList.contains("slider__image--current")) {
-      sliderImage[1].classList.remove("slider__image--current");
-      sliderImage[0].classList.add("slider__image--current");
-    };
-  }
-})
-
-
-
-
